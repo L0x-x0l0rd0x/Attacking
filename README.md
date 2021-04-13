28084ba2028c3780a76a76d22ca6f3f6453bf6db
Anonymous-xLORDx <77666333+L0x-x0l0rd0x@users.noreply.github.com>
2021-04-13 13:00:46 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+To Run Script :-
+1) python2 requirement.py
+2) python2 x0l0rd0x 
+This Script Was Maded By python2
+In This Tool You Have Multi Chose Like Dos Attack On Site With Port 
+Or Dos Attack On Ip With Port 
+Or Getting Website Ip Just With Name Of The Site
+You Can't Cause Damage In Any Site Or (Ip) Just With One Dos Attack
+But You Need Multi Attacks
---
+++ requirement.py
@@ -0,0 +1,14 @@
+import os
+import time
+os.system("toilet -f big 'REQUIREMENT' -F gay")
+time.sleep(2)
+os.system("sudo apt update")
+os.system("apt update")
+os.system("sudo apt upgrade")
+os.system("apt upgrade")
+os.system("sudo apt install toilet")
+os.system("apt install toilet")
+os.system("sudo apt install python2")
+os.system("apt install python2")
+os.system("sudo apt install figlet")
+os.system("apt install figlet")
